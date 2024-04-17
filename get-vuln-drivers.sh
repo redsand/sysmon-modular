98fd37c55385ffa821e844b07a0273db72f68046
Tim Shelton <tshelton@hawkdefense.com>
2024-04-17 21:17:21 +0000

    Includes vuln drivers

---
+++ get-vuln-drivers.sh
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/magicsword-io/LOLDrivers/main/detections/sysmon/sysmon_config_vulnerable_hashes.xml > 29_file_executable_detected/sysmon_config_vulnerable_hashes.xml
